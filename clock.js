6442223057c861c94cd92eb540db3337e85c9b02
Kevin Bisner <34324949+KBiz65@users.noreply.github.com>
2020-10-27 16:20:20 -0700

    added 12/24 time toggle button

--- clock.js
+++ clock.js
@@ -1,8 +1,23 @@
-setInterval(function getTime() {
-  const currentDate = new Date();
+function getTime12() {
+  setInterval(function () {
+    const currentDate = new Date();
 
-  document.getElementById("clockDate").innerHTML = currentDate.toDateString();
-  document.getElementById(
-    "clockTime"
-  ).innerHTML = currentDate.toLocaleTimeString();
-}, 1000);
+    document.getElementById("clockDate").innerHTML = currentDate.toDateString();
+    document.getElementById(
+      "clockTime"
+    ).innerHTML = currentDate.toLocaleTimeString();
+    document.getElementById("timeToggle").onclick = getTime24;
+  }, 1000);
+}
+
+function getTime24() {
+  setInterval(function () {
+    const currentDate = new Date();
+
+    document.getElementById("clockDate").innerHTML = currentDate.toDateString();
+    document.getElementById(
+      "clockTime"
+    ).innerHTML = currentDate.toLocaleTimeString("en-GB");
+    document.getElementById("timeToggle").onclick = getTime12;
+  }, 1000);
+}
