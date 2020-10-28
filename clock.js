972842c8ff1e10f56ade73d24ba1ba4908150640
Kevin Bisner <34324949+KBiz65@users.noreply.github.com>
2020-10-27 21:42:14 -0700

    added some documentation

--- clock.js
+++ clock.js
@@ -4,8 +4,9 @@ window.onload = getTime(); //runs function on page load
 
 function getTime() {
   setInterval(function () {
-    const currentDate = new Date();
+    const currentDate = new Date(); //establishes current date/time
 
+    //checks to see if user wants to show in military time or not
     if (isMilitary === true) {
       document.getElementById(
         "clockDate"
@@ -24,6 +25,7 @@ function getTime() {
   }, 1000);
 }
 
+//this is the event listener to watch for time toggle request
 document.getElementById("timeToggle").addEventListener("click", function () {
   if (isMilitary === false) {
     isMilitary = true;
